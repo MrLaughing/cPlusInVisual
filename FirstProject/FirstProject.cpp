330ff83eba95e78afe85ad3b4467ac781063ff03
Laughing_Lz <645596034@qq.com>
2016-05-05 20:27:49 +0800

    first提交项目到git

---
+++ FirstProject/FirstProject.cpp
@@ -0,0 +1,17 @@
+// FirstProject.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+using namespace std;
+int main()
+{
+	cout << "hello world" << endl;
+	int x = 100;
+	int *f, **p;
+	f = &x;
+	p = &f;
+	cout << "x=" << x << ",*f=" << *f << ",**p=" << **p << "." << endl;
+	return 0;
+}
