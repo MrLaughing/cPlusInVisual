fd3dd2ce1cdd3f9e9fbb257f83c9ac451cb87ad9
Laughing_Lz <645596034@qq.com>
2016-05-06 13:04:35 +0800

    指针与数组/字符串

--- FirstProject/FirstProject.cpp
+++ FirstProject/FirstProject.cpp
@@ -13,8 +13,19 @@ void test() {
 }
 /*指针的基本使用*/
 void point() {
-	int x[6] = { 1,2,3,4,5,6 }, *f;
-	f = x;//x代表数组的首地址
+	int x[6] = { 1,2,3,4,5,6 }, z[6],*f,*m,*n;
+	f = m = x;//x代表数组的首地址
+	n = z;
+	for (int i = 0; i < 6;i++) {
+		//*n++ = *f++ + *m++;
+		*n = *f + *m;
+		f++, m++,n++;//同上面代码
+	}
+	n = z;//重新将指针n指向数组z首地址
+	for (int i = 0; i < 6;i++) {
+		cout <<setw(3)<< *n++;
+	}
+	cout << endl;
 	if (*(&x[5]) == x[5]) {
 		cout <<setw(15)<<"*(地址) 可引用数组元素"<<endl;
 	}
@@ -35,10 +46,34 @@ void pointToReverse() {
 	}
 	cout << endl;
 }
+/*指针与二维数组*/
+void pointToArrays() {
+	int m[2][3] = {1,2,3,4,5,6};
+	int *f = m[0];//定义的是指针变量
+	for (int i = 0; i < 6;i++,f++) {
+		cout << setw(3)<<*f;
+	}
+	cout << endl;
+	int *a[2] = { m[0],m[1] };//定义的是指针数组
+	for (int i = 0; i < 2;i++) {
+		for (int j = 0; j < 3;j++) {
+			cout << setw(3)<<*(a[i] + j);//★
+		}
+	}
+	cout << endl;
+	int(*b)[3] = m;//定义的是行指针
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout <<setw(3)<<b[i][j];//★
+		}
+	}
+	cout << endl;
+}
 int main()
 {
-	pointToReverse();
-	//point();
+	pointToArrays();
+	//pointToReverse();
+	//();
 	//test();
 	return 0;
 }
